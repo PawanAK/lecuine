cce716bbae36473079a860d994c048014cd588cb
PawanAK <pawanajjark@gmail.com>
2024-02-17 11:55:56 +0530

    Sign In

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,48 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 
-const User = require('../models/userModel');
+const User = require('../models/user');
 
 // sign up
 router.post('/register', async (req, res) => {
     try {
         const { email, userName, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
 
         // Create a new user instance
-        const user = new User({ email, userName, password });
+        const user = new User({ email, userName, password: hashedPassword });
 
         // Save the user to the database
-        await user.save();
+        await user.save()
 
         // Respond with the created user details
-        res.status(200).json({ email, userName, password });
+        res.status(201).json({ user });
+
     } catch (error) {
         // Handle errors
-        res.status(400).json({ error: error.message });
+        res.status(400).json({ message: "user already exist" });
+    }
+});
+
+//Sign in
+
+router.post('/signin', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(400).json({ message: "Plz register first" });
+        }
+
+        const isPasswordCorrect = await bcrypt.compareSync(req.body.password, user.password);
+
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid password" });
+        }
+
+        const { password, ...others } = user._doc;
+        res.status(200).json({ others });
+    } catch (error) {
+        res.status(500).json({ message: "user Not Found" });
     }
 });
 
