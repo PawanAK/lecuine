1676383533b8c9caa61f5cf6473cae589e366221
PawanAK <pawanajjark@gmail.com>
2024-02-17 15:37:52 +0530

    feat: :sparkles: user Register

--- client/src/components/signup/Signup.jsx
+++ client/src/components/signup/Signup.jsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./index.css";
 
 const Signup = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [userName, setUserName] = useState("");
+  const [Inputs, setInputs] = useState({
+    email: "",
+    userName: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({ ...Inputs, [name]: value });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    await axios.post("http://localhost:4000/signup", Inputs);
+
+    console.log("Submitted data:", Inputs);
+    setInputs({ email: "", userName: "", password: "" });
   };
 
   return (
@@ -17,25 +29,28 @@ const Signup = () => {
       <label>Email:</label>
       <input
         type="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
+        name="email"
+        onChange={handleChange}
+        value={Inputs.email}
       />
 
       <label>UserName:</label>
       <input
         type="text"
-        onChange={(e) => setUserName(e.target.value)}
-        value={userName}
+        name="userName"
+        onChange={handleChange}
+        value={Inputs.userName}
       />
 
       <label>Password:</label>
       <input
         type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
+        name="password"
+        onChange={handleChange}
+        value={Inputs.password}
       />
 
-      <button>Sign Up</button>
+      <button type="submit">Sign Up</button>
     </form>
   );
 };
